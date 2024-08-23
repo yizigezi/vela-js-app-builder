f9f306caa184823d4580e8c3f91d65e88523f7f1
yizigezi <54109315+yizigezi@users.noreply.github.com>
2024-08-23 22:15:34 +0800

    a

--- index.js
+++ index.js
@@ -5,29 +5,9 @@ async function run() {
     try {
         const workspace = process.env.GITHUB_WORKSPACE;
 
-        const commands = `
-            cd /github/workspace
-            source /root/.bashrc
-            nvm use 20
-            npm i
-            npm run build
-        `;
-
-        // Run your Docker container
-        await exec.exec('docker', [
-            'pull',
-            '2minrain/devela:v2'
-        ]);
-        await exec.exec('docker', [
-            'run',
-            '--rm',
-            '-v',
-            `${workspace}:/github/workspace`,
-            `2minrain/devela:v2`,
-            '/bin/bash',
-            '-c',
-            commands
-        ]);
+        await exec.exec('cd', [workspace]);
+        await exec.exec('npm', ['i']);
+        await exec.exec('npm', ['run', 'build']);
     }
     catch (error) {
         core.setFailed(error.message);
