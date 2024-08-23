5e4d147ccda6edd68cf3674f33f98581a6e07825
yizigezi <54109315+yizigezi@users.noreply.github.com>
2024-08-23 21:15:44 +0800

    dev

--- index.js
+++ index.js
@@ -3,30 +3,12 @@ const exec = require('@actions/exec');
 
 async function run() {
     try {
-        let imageVersion = core.getInput('docker-version');
-        const location = core.getInput('location') || '';
-        const instance = core.getInput('instance');
-        const artifact = core.getInput('artifact');
         const workspace = process.env.GITHUB_WORKSPACE;
 
-        // Set a default docker image if docker-version is undefined
-        if (!imageVersion) {
-            imageVersion = '232.10275';
-        }
-
         const commands = `
-            export DISPLAY=:99
-            Xvfb :99 &
-            git config --global --add safe.directory /github/workspace
-            /opt/builder/bin/idea.sh helpbuilderinspect -source-dir /github/workspace/${location} -product ${instance} --runner github -output-dir /github/workspace/artifacts/ || true
-            echo "Test existing artifacts"
-            test -e /github/workspace/artifacts/${artifact} && echo ${artifact} exists
-            if [ -z "$(ls -A /github/workspace/artifacts/ 2>/dev/null)" ]; then
-               echo "Artifacts not found" && false
-            else
-               chmod 777 /github/workspace/artifacts/
-               ls -la /github/workspace/artifacts/
-            fi
+            cd /github/workspace
+            npm i
+            npm run build
         `;
 
         // Run your Docker container
@@ -35,7 +17,7 @@ async function run() {
             '--rm',
             '-v',
             `${workspace}:/github/workspace`,
-            `registry.jetbrains.team/p/writerside/builder/writerside-builder:${imageVersion}`,
+            `2minrain/devela:v2`,
             '/bin/bash',
             '-c',
             commands
